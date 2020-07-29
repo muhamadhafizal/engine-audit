49ab25395a0f09c3203a67be8dae43c41b1dd7a0
muhamadhafizal <ijaldev16@gmail.com>
2020-07-29 12:56:34 +0800

    fix:login details

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 use App\User;
+use App\Role;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
@@ -12,6 +13,8 @@ class LoginController extends Controller
     }
 
     public function main(Request $request){
+
+        $tempuser = array();
 
         $validator = validator::make($request->all(),
         [
@@ -31,11 +34,52 @@ class LoginController extends Controller
                     $a->where('username',$username)
                       ->orWhere('email',$username);
                 })
-                ->first();
+                ->first(); 
 
             if($user){
+
+                $roledetails = Role::where('id',$user->role)->first();
+                $rolename = $roledetails->name; 
+                
+                if($user->role == 2){
+                    $name = $user->name;
+
+                    $tempuser = [
+                        'id' => $user->id,
+                        'name' => $name,
+                        'contact' => $user->contact,
+                        'email' => $user->email,
+                        'role' => $rolename,
+                        'username' => $user->username,
+                        'address' => $user->address,
+                        'postalcode' => $user->postalcode,
+                        'location' => $user->location,
+                        'area' => $user->area,
+    
+                    ];
+                } else if($user->role == 3){
+                    $name = $user->name .' '.$user->lastname;
+
+                    $companydetails = User::where('id',$user->companyid)->first();
+                    $companyname = $companydetails->name;
+
+                    $tempuser = [
+                        'id' => $user->id,
+                        'name' => $name,
+                        'contact' => $user->contact,
+                        'email' => $user->email,
+                        'role' => $rolename,
+                        'username' => $user->username,
+                        'status' => $user->status,
+                        'companyname' => $companyname,
+    
+                    ];
+                }
+
+                
+
                 $token = $user->createToken('MyApp')-> accessToken;
-                return response()->json(['status'=>'success', 'api_key'=>$token, 'value'=>$user]);
+                return response()->json(['status'=>'success', 'api_key'=>$token, 'value'=>$tempuser]);
             } else {
                 return response()->json(['status'=>'failed']);
             }
